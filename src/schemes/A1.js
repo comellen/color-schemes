e1b44680ea19ee5d14b7da57707a8f26534c53e9
Connor Mellencamp <comellen91@gmail.com>
2019-02-12 13:14:39 -0500

    changed navbar

--- src/schemes/A1.js
+++ src/schemes/A1.js
@@ -5,18 +5,20 @@ import './A1.css';
 const A1 = () => {
     return (
         <Fade left cascade>
-            <div className="colorBar" id="red"></div>
-            <div className="colorBar" id="orange"></div>
-            <div className="colorBar" id="lightyellow"></div>
-            <div className="colorBar" id="green"></div>
-            <div className="colorBar" id="blue"></div>
-            <div className="colorBar" id="purple"></div>
-            <div className="colorBar" id="pink"></div>
-            <div className="colorBar" id="offwhite"></div>
-            <div className="colorBar" id="lightgrey"></div>
-            <div className="colorBar" id="grey"></div>
-            <div className="colorBar" id="black2"></div>
-            <div className="colorBar" id="black1"></div>
+            <div>
+                <div className="colorBar" id="red"></div>
+                <div className="colorBar" id="orange"></div>
+                <div className="colorBar" id="lightyellow"></div>
+                <div className="colorBar" id="green"></div>
+                <div className="colorBar" id="blue"></div>
+                <div className="colorBar" id="purple"></div>
+                <div className="colorBar" id="pink"></div>
+                <div className="colorBar" id="offwhite"></div>
+                <div className="colorBar" id="lightgrey"></div>
+                <div className="colorBar" id="grey"></div>
+                <div className="colorBar" id="black2"></div>
+                <div className="colorBar" id="black1"></div>
+            </div>
         </Fade>
     );
 }
--- src/schemes/Pamplemousse.js
+++ src/schemes/Pamplemousse.js
@@ -5,12 +5,14 @@ import './Pamplemousse.css';
 const Pamplemousse = () => {
     return (
         <Fade left cascade>
-            <div className="colorBar" id="grapefruitLC"></div>
-            <div className="colorBar" id="pinkLC"></div>
-            <div className="colorBar" id="yellowLC"></div>
-            <div className="colorBar" id="tealLC"></div>
-            <div className="colorBar" id="blueLC"></div>
-            <div className="colorBar" id="navyLC"></div>
+            <div>
+                <div className="colorBar" id="grapefruitLC"></div>
+                <div className="colorBar" id="pinkLC"></div>
+                <div className="colorBar" id="yellowLC"></div>
+                <div className="colorBar" id="tealLC"></div>
+                <div className="colorBar" id="blueLC"></div>
+                <div className="colorBar" id="navyLC"></div>
+            </div>
         </Fade>
     );
 }
